a826dc43f5f991e5993ce1ab53e6a17968611b46
NagiNikaido <naginikaido@kuusouhakuchuu.cn>
2024-09-24 09:40:32 +0800

    Bump version to 0.1.0

--- src/utils.lisp
+++ src/utils.lisp
@@ -21,7 +21,7 @@
 (in-package #:nitory)
 
 ;; managed by version bumper
-(defparameter +version+ "0.0.9")
+(defparameter +version+ "0.1.0")
 (setf *random-state* (make-random-state t))
 (setf re:*allow-named-registers* t)
 (setf v:*process-locally* t)
